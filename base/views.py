ef4df24091cb7d4cd094087dd2c339d052751dd4
Yaphet Gebrecristos <yaphet10@gmail.com>
2024-08-24 22:07:31 -0400

    Added some simple css styling

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -11,8 +11,10 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q')
+    rooms = Room.objects.filter(topic__name=q)
+    topics = Topic.objects.all()
+    context = {'rooms':rooms,'topics':topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -30,3 +32,23 @@ def createRoom(request):
         
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
