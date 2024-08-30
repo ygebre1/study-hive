a4066ea770951348d65589baca11482bcdfdb53e
Yaphet Gebrecristos <yaphet10@gmail.com>
2024-08-30 16:56:19 -0400

    Issues with creating user

--- base/models.py
+++ base/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from .managers import CustomUserManager  # Import the custom user manager
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-
-    avatar = models.ImageField(null=True, default="avatar.svg") 
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()  # Use the custom manager
+
     def __str__(self) -> str:
-        return self.username
+        return self.email
 
 
 # Create your models here.
