c344b503bba1608a92a206a23e04d7fd037f0ef0
Yaphet Gebrecristos <yaphet10@gmail.com>
2024-08-30 19:19:57 -0400

    Updates to the setting.py file

--- studyhive/settings.py
+++ studyhive/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-new-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.189.6.41']
 
